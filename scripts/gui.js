975df660826d9b580bbf91ba48bd2a0103b168d0
James <james@geekwagon.net>
2013-11-09 20:14:08 -0600

    url with hash now works with back button.

--- scripts/gui.js
+++ scripts/gui.js
@@ -1,30 +1,15 @@
-//Refreshes section
-function DisplaySection(clicked) {
-    //get showing section
-    displayed = '';
-    $('section').each(function() {
-        if($(this).css('display') == 'block') {
-            displayed = $(this).attr('id');
-        }
-    });
-    
-    //swap to clicked on section
-    $('#' + displayed).hide(0, function () {
-        $('#' + clicked).fadeIn(600)
-    });
-    
-    location.hash = clicked;
-    window.scrollTo(0, 0);
-}
-
 $(document).ready(function() {
-    //On load hide all except main.
-    $('section').hide();
-    if(location.hash != "") {
-        $(location.hash).show();
-    } else {
-        $('#main').show();
-    }
+    //display page based on hash
+    var pages = $('section');
+    pages.hide().filter(':first').show();
+
+    $(window).bind('hashchange', function () {
+        var hash = window.location.hash || '#main';
+        pages.hide();
+        pages.filter(hash).fadeIn(600);
+    });
+
+    $(window).trigger('hashchange');
     
     //copyright year
     var now = new Date();
@@ -36,6 +21,7 @@ $(document).ready(function() {
     $('.menu-item').click(function() {
         $('.menu-item').removeClass('selected');
         $(this).addClass('selected');
-        DisplaySection($(this).attr('id').slice(3));
+        window.location.hash = $(this).attr('id').slice(3);
     })
+
 });
